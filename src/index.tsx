2ea8a082090117b92e9f2d6f9cac0d555c5ae54c
mahmud1995 <makhmudgo95@gmail.com>
2024-10-10 20:55:40 +0900

    fix: Modify Root Index Render Logic2

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import { Provider } from 'react-redux';
 import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
@@ -10,21 +10,29 @@ import './css/index.css';
 import theme from './app/MaterialTheme';
 import { BrowserRouter as Router } from 'react-router-dom';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <ThemeProvider theme={theme}>
-        <CssBaseline />
-        <Router>
-          <App />
-        </Router>
-      </ThemeProvider>
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const container = document.getElementById('root');
+
+if (container) {
+  const root = createRoot(container);
+  root.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Router>
+            <App />
+          </Router>
+        </ThemeProvider>
+      </Provider>
+    </React.StrictMode>
+  );
+
+} else {
+  console.log("Root element not found!");
+}
+reportWebVitals();
+/*This ensures that createRoot is only called if the container is found, 
+and if not, it will log an error to the console. 
+This way, you avoid trying to render the app when the root element doesn't exist.*/
 
 // If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
