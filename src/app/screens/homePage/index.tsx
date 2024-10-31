780df4ee5e65bdfb7c64d92536769490d1575f2f
mahmud1995 <makhmudgo95@gmail.com>
2024-11-01 06:42:29 +0900

    feat: Create Config & env variables

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -7,8 +7,11 @@ import ActiveUser from "./ActiveUser";
 import Events from "./Events";
 import "../../../css/home.css"
 
+
 // import './css/home.css'
 export default function HomePage() {
+
+
     return <div className={"home-page"}>
         <Statistics />
         <PopularDishes />
